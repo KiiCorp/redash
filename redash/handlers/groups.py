b499acf39bcf2b8d753397641ccbfbda72ae954b
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-10-17 15:49:02 +0900

    Implement changing permissions REST API.

--- redash/handlers/groups.py
+++ redash/handlers/groups.py
@@ -97,6 +97,25 @@ class GroupMemberListResource(BaseResource):
         members = models.Group.members(group_id)
         return [m.to_dict() for m in members]
 
+class GroupPermissionListResource(BaseResource):
+    @require_admin
+    def post(self, group_id):
+        group = models.Group.get_by_id_and_org(group_id, self.current_org)
+
+        if group.type == models.Group.BUILTIN_GROUP:
+            abort(400, message="Can't modify built-in groups.")
+
+        group.permissions = request.json
+        models.db.session.commit()
+
+        self.record_event({
+            'action': 'edit',
+            'timestamp': int(time.time()),
+            'object_id': group.id,
+            'object_type': 'group'
+        })
+
+        return group.to_dict()
 
 class GroupMemberResource(BaseResource):
     @require_admin
