84dfa3919763545bafe98483e97663a89dd9cbfc
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-12-04 18:55:30 +0900

    Fix.

--- redash-setup.sh
+++ redash-setup.sh
@@ -4,5 +4,5 @@ set -eu
 
 python /app/redash-setup.py wait
 /app/bin/docker-entrypoint create_db
-nohup /app/bin/docker-entrypoint server &
+nohup /app/bin/docker-entrypoint server >/dev/null 2>&1 &
 python /app/redash-setup.py bootstrap
