9b12dae7496146f120b360007c2d64e00b19c41b
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-12-10 16:38:33 +0900

    Fix Dockerfile.

--- redash-setup.sh
+++ redash-setup.sh
@@ -2,5 +2,5 @@
 
 set -eu
 
-python /app/redash-setup.py setup --name="${REDASH_ADMIN_NAME}" --email="${REDASH_ADMIN_EMAIL}$" --password="${REDASH_ADMIN_PASSWORD}"
+python /app/redash-setup.py setup --name="${REDASH_ADMIN_NAME}" --email="${REDASH_ADMIN_EMAIL}" --password="${REDASH_ADMIN_PASSWORD}"
 exec /app/bin/docker-entrypoint server 
