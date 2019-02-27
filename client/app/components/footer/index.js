999baf14691ac4d0f0342ce4b450418212f324c8
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2019-02-27 16:46:32 +0900

    Enable to confirm varanus redash version.

--- client/app/components/footer/index.js
+++ client/app/components/footer/index.js
@@ -2,6 +2,7 @@ import template from './footer.html';
 
 function controller(clientConfig, currentUser) {
   this.version = clientConfig.version;
+  this.varanus_redash_version = clientConfig.varanus_redash_version;
   this.newVersionAvailable = clientConfig.newVersionAvailable && currentUser.isAdmin;
 }
 
