f268d014721b8d03020a06d9f5c2c35fe4392b29
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2019-11-13 18:43:42 +0900

    Remove redundant redash-setup.sh.

--- redash-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-python /app/redash-setup.py setup --name="${REDASH_ADMIN_NAME}" --email="${REDASH_ADMIN_EMAIL}" --password="${REDASH_ADMIN_PASSWORD}"
-exec /app/bin/docker-entrypoint server 
